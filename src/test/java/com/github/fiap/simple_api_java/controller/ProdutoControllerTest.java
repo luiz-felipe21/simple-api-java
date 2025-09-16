6ea1db379a9731fc9f31d39bf47d8c04f87a55b7
Luiz Felipe <luizfelipernunes21@gmail.com>
2025-09-16 16:08:51 -0300

    Adicionando CI

--- src/test/java/com/github/fiap/simple_api_java/controller/ProdutoControllerTest.java
+++ src/test/java/com/github/fiap/simple_api_java/controller/ProdutoControllerTest.java
@@ -57,7 +57,7 @@ public class ProdutoControllerTest {
     @DisplayName("test fail")
     @Test
     void testGivenNewProduto_whenCreate_thenFail() throws Exception{
-        // fail("Um erro acontecerá");
+         //fail("Uma nova falha");
     }
     
 }
